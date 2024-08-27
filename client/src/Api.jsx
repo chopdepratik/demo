cf0f3bc3bb7cef14483fe051c11e9265a3d82bb3
Pratik Chopde <chopdepratik60@gmail.com>
2024-08-27 21:18:51 +0530

    Update Api.jsx

--- client/src/Api.jsx
+++ client/src/Api.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'https://management-o5hr.onrender.com';
-const API_URL2 = 'https://management-o5hr.onrender.com'; // Update with your backend URL
-const API_URL3 = 'https://management-o5hr.onrender.com'
+const API_URL = 'https://management-o5hr.onrender.com/api/v1/business';
+const API_URL2 = 'https://management-o5hr.onrender.com/api/v2/business';
+const API_URL3 = 'https://management-o5hr.onrender.com/api/v3/business';
 
 // Register a new user
 export const registerUser = async (userData) => {
@@ -13,31 +13,42 @@ export const registerUser = async (userData) => {
     throw error.response.data;
   }
 };
-export const registeremp = async (empData) => {
-    try {
-      const response = await axios.post(`${API_URL2}/registeremp`, empData);
-      return response.data;
-    } catch (error) {
-      throw error.response.data;
-    }
-  };
-  export const registerTask = async (taskData) => {
-    try {
-      const response = await axios.post('https://management-o5hr.onrender.com/registertask', taskData);
-      return response.data;
-    } catch (error) {
-      throw error.response.data; // Throw the actual error response for debugging
-    }
-  };
 
-export const registerProject= async (taskData) => {
-  const response = await axios.post('https://management-o5hr.onrender.com/registerproject', taskData);
-  return response.data;
+export const registerEmp = async (empData) => {
+  try {
+    const response = await axios.post(`${API_URL2}/registeremp`, empData);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
 };
+
+export const registerTask = async (taskData) => {
+  try {
+    const response = await axios.post(`${API_URL3}/registertask`, taskData);
+    return response.data;
+  } catch (error) {
+    throw error.response.data; // Throw the actual error response for debugging
+  }
+};
+
+export const registerProject = async (taskData) => {
+  try {
+    const response = await axios.post(`${API_URL3}/registerproject`, taskData);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
 // Login user
 export const loginUser = async (userData) => {
   try {
     const response = await axios.post(`${API_URL}/login`, userData);
+    const token = response.data.token; // Assuming the token is returned as part of response.data
+    if (token) {
+      localStorage.setItem('token', token); // Store the token in localStorage
+    }
     return response.data;
   } catch (error) {
     throw error.response.data;
