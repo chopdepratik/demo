dae92ef277c0acc9a9f6fe1caed6e2b82afad74b
Pratik Chopde <chopdepratik60@gmail.com>
2024-08-27 17:28:40 +0530

    Update employee.middleware.js

--- Server/middleware/employee.middleware.js
+++ Server/middleware/employee.middleware.js
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 import Emp from "../models/employee.models.js";
 
-// Function to create a JWT token for an employee
 export const createToken = (empId, empEmail) => {
     const token = jwt.sign(
         {
@@ -13,16 +12,14 @@ export const createToken = (empId, empEmail) => {
             expiresIn: "5d",
         }
     );
+
     return token;
 };
 
-// Middleware to check if the user is authenticated
-export const isAuthenticated = async (req, res, next) => {
+export const isAuthenticated = (req, res, next) => {
     try {
-        // Extract token from the Authorization header
         const token = req.headers.authorization?.split(" ")[1];
 
-        // Check if the token is missing
         if (!token) {
             return res.status(401).json({
                 success: false,
@@ -31,37 +28,22 @@ export const isAuthenticated = async (req, res, next) => {
             });
         }
 
-        // Verify the token and extract user information
-        jwt.verify(token, process.env.SECRET, async (err, decoded) => {
+        jwt.verify(token, process.env.SECRET, async (err, user) => {
             if (err) {
                 return res.status(400).json({
                     success: false,
                     isLogin: false,
-                    message: "Invalid or expired token",
+                    message: err.message,
                 });
             }
 
-            // Fetch the employee details from the database
-            const employee = await Emp.findById(decoded.empId);
-
-            // Check if the employee exists in the database
-            if (!employee) {
-                return res.status(404).json({
-                    success: false,
-                    isLogin: false,
-                    message: "Employee not found",
-                });
-            }
-
-            // Attach the employee details to the request object
-            req.user = employee;
-            next(); // Proceed to the next middleware or route handler
+            req.user = await Emp.findById(user.empId);
+            next();
         });
     } catch (err) {
-        // Handle unexpected errors
         res.status(500).json({
             success: false,
-            message: "Internal Server Error: " + err.message,
+            message: err.message,
         });
     }
 };
