779fd40683c81084ccf5bc69ed307b5b92cb114f
Pratik Chopde <chopdepratik60@gmail.com>
2024-08-27 17:29:12 +0530

    Update auth.middleware.js

--- Server/middleware/auth.middleware.js
+++ Server/middleware/auth.middleware.js
@@ -12,13 +12,13 @@ export const createToken = (id, email) => {
             expiresIn: "5d",
         }
     );
+
     return token;
 };
 
-export const isAuthenticated = async (req, res, next) => {
+export const isAuthenticated = (req, res, next) => {
     try {
-        const authHeader = req.headers.authorization;
-        const token = authHeader && authHeader.split(" ")[1];
+        const token = req.headers.authorization?.split(" ")[1];
 
         if (!token) {
             return res.status(401).json({
@@ -28,32 +28,22 @@ export const isAuthenticated = async (req, res, next) => {
             });
         }
 
-        jwt.verify(token, process.env.SECRET, async (err, decoded) => {
+        jwt.verify(token, process.env.SECRET, async (err, user) => {
             if (err) {
                 return res.status(400).json({
                     success: false,
                     isLogin: false,
-                    message: "Invalid or expired token",
+                    message: err.message,
                 });
             }
 
-            const user = await User.findById(decoded.id);
-
-            if (!user) {
-                return res.status(404).json({
-                    success: false,
-                    isLogin: false,
-                    message: "User not found",
-                });
-            }
-
-            req.user = user; // Add user to the request object
-            next(); // Call the next middleware or route handler
+            req.user = await User.findById(user.id);
+            next();
         });
     } catch (err) {
         res.status(500).json({
             success: false,
-            message: "Internal Server Error: " + err.message,
+            message: err.message,
         });
     }
-};
+}; 
